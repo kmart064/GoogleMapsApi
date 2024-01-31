5ca6ca62fa796e029e184942f0c000cc795792dd
kmart064 <jg>
2024-02-01 01:14:56 +0900

    Added additional location details (phone number, website, etc.) to the infoWindow of the marker when clicked

--- index.ts
+++ index.ts
@@ -1,5 +1,13 @@
 let infoWindow: google.maps.InfoWindow;
 let map: google.maps.Map;
+enum PriceLevel {
+  Free = 0,
+  Inexpensive,
+  Moderate,
+  Expensive,
+  Very_Expensive,
+  Unknown
+}
 
 async function initMap(): Promise<void> {
   // Request needed libraries.
@@ -21,30 +29,51 @@ async function initMap(): Promise<void> {
 
   var request = {
 		query: 'restaurant',
-		fields: ['name', 'formatted_address', 'types', 'geometry', 'place_id'],
+		fields: ['geometry', 'place_id'],
   };
 
   let service = new PlacesService(map);
-	service.textSearch(request, function(results, status) {
-		if (status === google.maps.places.PlacesServiceStatus.OK) {
-			for (var i = 0; i < results.length; i++) {
-				createMarker(results[i], infoWindow);
+	const textSearchPromise = new Promise((resolve, reject) => {
+		service.textSearch(request, function(results, status) {
+			if (status === google.maps.places.PlacesServiceStatus.OK) {
+				for (var i = 0; i < results.length; i++) {
+					createMarker(results[i], infoWindow);
+				}
+				map.setCenter(results[0].geometry.location);
+				resolve(results);
 			}
-			map.setCenter(results[0].geometry.location);
-		}
+			else {
+				reject("The call to the PlacesAPI textSearch failed with code: " + status);
+			}
+		});
+	});
+	textSearchPromise.then((res: google.maps.places.PlaceResult[]) => {
+    for (let i = 0; i < res.length; i++) {
+      var request = {
+        placeId: res[i].place_id,
+        fields: ['name', 'formatted_address', 'formatted_phone_number', 'website', 'price_level', 'geometry', 'place_id'],
+      };
+      service.getDetails(request, function(result, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          createMarker(result, infoWindow);
+        }
+      });
+    }
+  }, (error) => {
+		alert(error);
 	});
 }
 
 /**
- * Places a marker on the Map for a given PlaceResult.
- * Also adds an infoWindow to the marker that will pop up when
+ * Places a Marker on the Map for a given PlaceResult.
+ * Also adds an InfoWindow to the marker that will pop up when
  * the marker is clicked, containing information, such as
  * the name, address, and placeId for the place.
  * @param place A PlaceResult obtained from querying the PlacesAPI
  * 	for a specific place, which should contain at least the name,
  * 	placeId, formatted address, and geometry.
  */
-function createMarker(place: google.maps.places.PlaceResult, infoWindow) {
+function createMarker(place: google.maps.places.PlaceResult, infoWindow: google.maps.InfoWindow) {
 	if (!place.geometry || !place.geometry.location) return;
   
 	const marker = new google.maps.Marker({
@@ -59,19 +88,28 @@ function createMarker(place: google.maps.places.PlaceResult, infoWindow) {
     nameElement.textContent = place.name;
     content.appendChild(nameElement);
 
-    const placeIdElement = document.createElement("p");
-    placeIdElement.textContent = place.place_id;
-    content.appendChild(placeIdElement);
-
     const placeAddressElement = document.createElement("p");
     placeAddressElement.textContent = place.formatted_address;
     content.appendChild(placeAddressElement);
+		
+		const phoneNumberElement = document.createElement("p");
+    phoneNumberElement.textContent = place.formatted_phone_number;
+    content.appendChild(phoneNumberElement);
 
-    if (place.types) {
-      const placeTypeElement = document.createElement("p");
-      placeTypeElement.textContent = place.types.join(", ");
-      content.appendChild(placeTypeElement);
+    const websiteElement = document.createElement("p");
+    websiteElement.textContent = place.website;
+    content.appendChild(websiteElement);
+
+    const priceLevelElement = document.createElement("p");
+    let priceLevel: PriceLevel;
+    if (place.price_level) {
+      priceLevel = place.price_level;
     }
+    else {
+      priceLevel = PriceLevel.Unknown;
+    }
+		priceLevelElement.textContent = "Price level: " + PriceLevel[priceLevel];
+		content.appendChild(priceLevelElement);
     
     infoWindow.setContent(content);
 	  infoWindow.open({
